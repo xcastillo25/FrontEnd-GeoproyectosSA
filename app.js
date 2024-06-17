9f25a0dc4533bb689196dc509f03573e11416d70
xcastillo25 <xcastillo25@gmail.com>
2024-06-17 11:34:04 -0600

    modificado el dev

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.use((err, req, res, next) => {
 
 // Cabeceras CORS
 app.use((req, res, next) => {
-    // res.header('Access-Control-Allow-Origin', 'https://main--geoproyectossa.netlify.app');
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Origin', 'https://main--geoproyectossa.netlify.app');
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
